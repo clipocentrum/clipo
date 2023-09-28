73cf121067dd97be32bd096e70d59450d46ff7bd
Bartosz Zakrzewski <78502440+BartekZakrzewski@users.noreply.github.com>
2023-09-28 09:04:24 +0200

    Update specialist.tsx

--- components/specialist.tsx
+++ components/specialist.tsx
@@ -13,9 +13,9 @@ const Specialist = ({name, specialisation, key, slug}: Props) => {
     <div className={`specialist rounded-xl w-44 md:w-48 flex flex-col justify-center items-center `}>
         <div className={`specialist__photo w-40 h-40 rounded-full translate-y-10`} />
         <div className={`specialist__container flex flex-col text-white text-center bg-slate-700 w-full rounded-xl pt-10 pb-3`}>
-            <span className={`font-bold`}>Joanna Kowalska</span>
-            <span className={`italic opacity-80`}>Diabetolog</span>
-            <a href='#' className={`text-green-500 underline transition-opacity hover:opacity-90`}>Wiecej</a>
+            <span className={`font-bold`}>{ name }</span>
+            <span className={`italic opacity-80`}>{ specialisation }</span>
+            <a href={`specjalisci/${slug}`} className={`text-green-500 underline transition-opacity hover:opacity-90`}>Wiecej</a>
         </div>
     </div>
   )
