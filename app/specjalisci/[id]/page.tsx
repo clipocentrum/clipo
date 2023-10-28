f2dfc5b1b39ef512eb0150c6bdc1553733009376
Bartosz Zakrzewski <bartek.zakrzewski3012@gmail.com>
2023-10-28 13:43:03 +0200

    13:42 28.10.23

--- app/specjalisci/[id]/page.tsx
+++ app/specjalisci/[id]/page.tsx
@@ -10,7 +10,7 @@ function Specialista({params}: {params: {id: string}}){
         <>
             <section className={`container py-20 w-full flex flex-col items-center m-auto px-2`}>
                 <section className={`specialist__container relative grid grid-cols-1 lg:grid-cols-2 w-full place-items-center py-10`}>
-                    <Image src={`/photos/${specialist.photoPath}`} alt={specialist.fullName} className={`w-full aspect-square rounded-xl lg:translate-x-10 z-40`} width={500} height={500} />
+                    <Image src={`/photos/${specialist.photoPath}`} alt={specialist.fullName} className={`w-full aspect-auto rounded-xl lg:translate-x-10 z-40`} width={500} height={500} />
                     <div className={`bg-slate-700 w-full p-2 rounded-xl -translate-y-10 grid grid-col-1 lg:-translate-y-0 lg:-translate-x-10 lg:shadow-sm z-40`}>
                         <h1 className={`text-green-500 text-4xl font-bold text-center self-center`}>
                             <span className={`text-white opacity-50 font-normal italic`}>{specialist.title} </span>
